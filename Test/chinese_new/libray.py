a5e09931bc34506efab9d4e2ce405053e1ad91ff
yht <yht2004@126.com>
2021-04-15 14:56:43 +0800

    add libary

---
+++ Test/chinese_new/libray.py
@@ -0,0 +1,22 @@
+"""
+----------------------------------------------
+File Name:     libray.py
+Descriptions:
+Author:        yht
+date:          2021/04/15
+
+-----------------------------------------------
+"""
+import requests,json
+import unittest
+
+
+class TestLibray(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ common/db.py
@@ -0,0 +1,9 @@
+"""
+----------------------------------------------
+File Name:     db.py
+Descriptions:  mysql database contention
+Author:        yht
+date:          2021/04/15
+
+-----------------------------------------------
+"""
